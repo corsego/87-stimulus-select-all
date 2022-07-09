93be87e9b0a65b0afdd1ccd1782d7458e481c9ad
Yaro <yshmarov@gmail.com>
2022-07-09 15:16:55 +0200

    edit multiple

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.order(created_at: :desc)
+  end
+
+  def bulk_update
+    # binding.b
+    @selected_users = User.where(id: params.fetch(:user_ids, []).compact)
+    if params[:commit] == 'disabled'
+      @selected_users.update_all(status: :disabled)
+    elsif params[:commit] == 'active'
+      @selected_users.each { |u| u.active! }
+    end
+    flash[:notice] = "#{@selected_users.count} users marked as #{params[:commit]}"
+    redirect_to action: :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    collection do
+      patch :bulk_update
+    end
+  end
   root 'users#index'
 end
